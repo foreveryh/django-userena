ae071c38b5c20a897b8b5a24b7fd8af1f04ca2cd
tchen <tchen@tukeq.com>
2012-09-28 15:49:29 +0800

    make the invitation code size to 20

--- userena/contrib/invitation/settings.py
+++ userena/contrib/invitation/settings.py
@@ -5,7 +5,7 @@ INVITE_ONLY = getattr(settings, 'INVITATION_INVITE_ONLY', False)
 PENDING_LIMIT = getattr(settings, 'INVITATION_PENDING_LIMIT', 1)
 EXPIRE_DAYS = getattr(settings, 'INVITATION_EXPIRE_DAYS', 30)
 DEFAULT_FROM_EMAIL = getattr(settings, 'INVITATION_FROM_EMAIL', 'admin@tukeq.com')
-INVITE_CODE_SIZE = getattr(settings, 'INVITATION_CODE_SIZE', 8) #MUST BE LESS THAN 40
+INVITE_CODE_SIZE = getattr(settings, 'INVITATION_CODE_SIZE', 20) #MUST BE LESS THAN 40
 
 
 from userena.contrib.invitation.signals import import_signal
