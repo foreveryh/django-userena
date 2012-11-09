161fb3dbac0e97a3586e9fa50b42b598dfd7ec62
tchen <tchen@tukeq.com>
2012-11-09 08:55:24 +0800

    change content label

--- userena/contrib/invitation/forms.py
+++ userena/contrib/invitation/forms.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django import forms
 from models import InvitationRequest, InvitationCode
 
@@ -5,7 +7,7 @@ class InvitationRequestForm(forms.ModelForm):
     """
     User can offer an email for  requesting an invite code
     """
-    content = forms.CharField(widget=forms.Textarea())
+    content = forms.CharField(widget=forms.Textarea(), label='自我介绍')
     class Meta:
         model = InvitationRequest
         fields = ('email', 'content')
