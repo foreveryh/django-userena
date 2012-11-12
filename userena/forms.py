df302115f97b1754086348ccdd4d63b87e635343
tchen <tchen@tukeq.com>
2012-11-12 16:08:58 +0800

    remove user guide in setting page

--- userena/forms.py
+++ userena/forms.py
@@ -231,7 +231,7 @@ class EditProfileForm(forms.ModelForm):
 
     class Meta:
         model = get_profile_model()
-        exclude = ['user', 'nickname', 'slug', 'tags_history']
+        exclude = ['user', 'nickname', 'slug', 'tags_history', 'guides']
 
     def save(self, force_insert=False, force_update=False, commit=True):
         profile = super(EditProfileForm, self).save(commit=commit)
