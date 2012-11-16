f9a54577f4d8be72994fa2a0d9c0aecbdc60801a
chenchiyuan <chenchiyuan03@gmail.com>
2012-11-16 11:11:13 +0800

    exclude old_user field in profile page.

--- userena/forms.py
+++ userena/forms.py
@@ -231,7 +231,7 @@ class EditProfileForm(forms.ModelForm):
 
     class Meta:
         model = get_profile_model()
-        exclude = ['user', 'nickname', 'slug', 'tags_history', 'guides', 'birthday']
+        exclude = ['user', 'nickname', 'slug', 'tags_history', 'guides', 'birthday', 'old_user']
 
     def save(self, force_insert=False, force_update=False, commit=True):
         profile = super(EditProfileForm, self).save(commit=commit)
